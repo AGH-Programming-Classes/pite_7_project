0ee9362da73a672846054950285ac4bf0a6bd9ab
Jakub Jucha <kuba.jucha.24@gmail.com>
2026-01-11 21:22:25 +0100

    merge setting area and spawning food source ui

--- src/run.py
+++ src/run.py
@@ -5,7 +5,7 @@ from pygame_gui.elements import UIButton, UIPanel
 import config
 from environment import Environment
 from area import Area
-
+from food import FoodSource
 from ui import UI
 
 pygame.init()
@@ -21,49 +21,6 @@ env_ui = UI(
     width=config.WINDOW_WIDTH - (config.PANEL_WIDTH + 2*config.PANEL_X),
     height=config.WINDOW_HEIGHT
 )
-
-FOOD_COLORS = {
-    "Grass": (100, 200, 100),
-    "Berry": (150, 70, 160),
-    "Fruit": (125, 200, 50),
-    "Cactus": (190, 165, 50)
-}
-HERBIVORE_DOT = (0, 200, 0)
-
-sidebar_width = config.PANEL_X
-ui_panel = UIPanel(relative_rect=pygame.Rect(0, 0, sidebar_width, config.WINDOW_HEIGHT),
-                    manager=manager, starting_height=1)
-
-button_pairs = [
-    ("Grass", Area.PLAINS),
-    ("Berry", Area.BERRY_CORNER),
-    ("Fruit", Area.FERTILE_VALLEY),
-    ("Cactus", Area.DESERT)
-]
-
-ui_elements = []
-y_offset = 10
-btn_w = (sidebar_width - 30) // 2
-btn_h = (sidebar_width - 30) // 2
-
-for food_str, area_obj in button_pairs:
-    pair_color = area_obj.color
-
-    btn_food = UIButton(relative_rect=pygame.Rect(10, y_offset, btn_w, btn_h),
-                        text="", manager=manager, container=ui_panel)
-    btn_food.colours['normal_bg'] = pygame.Color(pair_color)
-    btn_food.rebuild()
-    ui_elements.append({"btn": btn_food, "value": food_str})
-
-    btn_area = UIButton(relative_rect=pygame.Rect(10 + btn_w + 10, y_offset, btn_w, btn_h),
-                        text="", manager=manager, container=ui_panel)
-    btn_area.colours['normal_bg'] = pygame.Color(pair_color)
-    btn_area.rebuild()
-    ui_elements.append({"btn": btn_area, "value": area_obj})
-
-    y_offset += btn_h + 10
-
-current_brush = None
 
 env = Environment(
     grid_width=config.GRID_WIDTH,
@@ -82,11 +39,6 @@ while running:
             env.shutdown()
             running = False
 
-        if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            for item in ui_elements:
-                if event.ui_element == item["btn"]:
-                    current_brush = item["value"]
-
         if event.type == pygame.MOUSEBUTTONDOWN:
             sim_rect = pygame.Rect(config.PANEL_X,
                                     config.PANEL_Y,
@@ -95,11 +47,11 @@ while running:
             if sim_rect.collidepoint(mouse_pos):
                 grid_x = (mouse_pos[0] - config.PANEL_X) // config.CELL_SIZE
                 grid_y = (mouse_pos[1] - config.PANEL_Y) // config.CELL_SIZE
-                if current_brush:
-                    if isinstance(current_brush, Area):
-                        env.change_area_at(grid_x, grid_y, current_brush)
-                    else:
-                        env.add_manual_food_source(grid_x, grid_y, current_brush)
+                if env_ui.current_brush:
+                    if isinstance(env_ui.current_brush, Area):
+                        env.change_area_at(grid_x, grid_y, env_ui.current_brush)
+                    elif isinstance(env_ui.current_brush, type(FoodSource)):
+                        env.add_manual_food_source(grid_x, grid_y, env_ui.current_brush)
         manager.process_events(event)
         env_ui.process_events(event, env)
 
@@ -115,17 +67,6 @@ while running:
     )
 
     manager.draw_ui(screen)
-
-    for item in ui_elements:
-        btn_rect = item["btn"].get_abs_rect()
-
-        if isinstance(item["value"], str):
-            dot_pos = btn_rect.center
-            pygame.draw.circle(screen, HERBIVORE_DOT, dot_pos, 7)
-            pygame.draw.circle(screen, (0, 0, 0), dot_pos, 7, 1)
-
-        if item["value"] == current_brush:
-            pygame.draw.rect(screen, (255, 255, 255), btn_rect, 3)
 
     pygame.display.flip()
 
--- src/ui.py
+++ src/ui.py
@@ -1,10 +1,12 @@
 import pygame
 import pygame_gui
 from environment import Environment
-from area import Area
+from area import Area, AREA_FOOD_SOURCE_MAPPING
+from agent import Agent
 
 class UI:
     def __init__(self, manager: pygame_gui.UIManager, x: int, y: int, width: int, height: int):
+        self.current_brush = None
         self.manager = manager
 
         margin = 10
@@ -24,11 +26,7 @@ class UI:
         self.spawn_ctrl_container = pygame_gui.elements.UIAutoResizingContainer(
             relative_rect=pygame.Rect(0, 0, self.panel.get_abs_rect().width - 2 * margin, 0),
             manager=self.manager,
-            container=self.panel,
-            resize_top=False,
-            resize_right=False,
-            resize_bottom=True,
-            resize_left=False
+            container=self.panel
         )
 
         self.spawn_label = pygame_gui.elements.UILabel(
@@ -42,7 +40,7 @@ class UI:
         )
 
         self.spawn_agent_btn = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect(0, margin, (self.spawn_ctrl_container.get_abs_rect().width - margin) / 2, btn_height),
+            relative_rect=pygame.Rect(0, margin, self.spawn_ctrl_container.get_abs_rect().width, btn_height),
             text='Agent',
             manager=self.manager,
             container=self.spawn_ctrl_container,
@@ -50,18 +48,40 @@ class UI:
                 'top_target': self.spawn_label
             }
         )
-        
-        self.spawn_food_source_btn = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect(margin, margin, (self.spawn_ctrl_container.get_abs_rect().width - margin) / 2, btn_height),
-            text='Food source',
+
+        self.spawn_food_source_container = pygame_gui.elements.UIAutoResizingContainer(
+            relative_rect=pygame.Rect(0, 0, self.spawn_ctrl_container.get_abs_rect().width, 0),
             manager=self.manager,
             container=self.spawn_ctrl_container,
             anchors={
-                'top_target': self.spawn_label,
-                'left_target': self.spawn_agent_btn
+                'top_target': self.spawn_agent_btn
             }
         )
 
+        self.spawn_food_source_btns = dict()
+        y_offset = margin
+        for area in list(Area):
+            btn = pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect(0, y_offset, btn_height, btn_height),
+                text='',
+                manager=self.manager,
+                container=self.spawn_food_source_container,
+
+            )
+            btn.colours['normal_bg'] = pygame.Color(area.color)
+            btn.rebuild()
+            label = pygame_gui.elements.UILabel(
+                relative_rect=pygame.Rect(margin, y_offset, -1, btn_height),
+                text=AREA_FOOD_SOURCE_MAPPING[area].__name__,
+                manager=self.manager,
+                container=self.spawn_food_source_container,
+                anchors={
+                    'left_target': btn
+                }
+            )
+            self.spawn_food_source_btns[btn] = AREA_FOOD_SOURCE_MAPPING[area]
+            y_offset += margin + btn_height
+        
         # set area ui section
         self.area_ctrl_container = pygame_gui.elements.UIAutoResizingContainer(
             relative_rect=pygame.Rect(0, margin, self.panel.get_abs_rect().width - 2 * margin, 200),
@@ -82,27 +102,39 @@ class UI:
             }
         )
 
-        self.area_btn = pygame_gui.elements.UIButton(
-            relative_rect=pygame.Rect(0, margin, (self.spawn_ctrl_container.get_abs_rect().width - margin) / 3, btn_height),
-            text='Set Area',
+        self.set_area_btns_container = pygame_gui.elements.UIAutoResizingContainer(
+            relative_rect=pygame.Rect(0, 0, self.area_ctrl_container.get_abs_rect().width, 0),
             manager=self.manager,
             container=self.area_ctrl_container,
             anchors={
                 'top_target': self.area_label
             }
         )
-        
-        self.area_list = pygame_gui.elements.UIDropDownMenu(
-            relative_rect=pygame.Rect(margin, margin, (self.spawn_ctrl_container.get_abs_rect().width - margin) / 3 * 2, btn_height),
-            options_list=[area.display_name for area in list(Area)],
-            starting_option=list(Area)[0].display_name,
-            manager=self.manager,
-            container=self.area_ctrl_container,
-            anchors={
-                'top_target': self.area_label,
-                'left_target': self.area_btn
-            }
-        )
+
+        self.set_area_btns = dict()
+        y_offset = margin
+        for area in list(Area):
+            btn = pygame_gui.elements.UIButton(
+                relative_rect=pygame.Rect(0, y_offset, btn_height, btn_height),
+                text='',
+                manager=self.manager,
+                container=self.set_area_btns_container,
+
+            )
+            btn.colours['normal_bg'] = pygame.Color(area.color)
+            btn.rebuild()
+            label = pygame_gui.elements.UILabel(
+                relative_rect=pygame.Rect(margin, y_offset, -1, btn_height),
+                text=area.display_name,
+                manager=self.manager,
+                container=self.set_area_btns_container,
+                anchors={
+                    'left_target': btn
+                }
+            )
+            self.set_area_btns[btn] = area
+            y_offset += margin + btn_height
+           
 
         # sim control ui section
         self.sim_ctrl_container = pygame_gui.core.UIContainer(
@@ -154,28 +186,41 @@ class UI:
 
         self.action_label = pygame_gui.elements.UILabel(
             relative_rect=pygame.Rect(0, 0, -1, -1),
-            text='Current action:',
+            text='Current brush:',
             manager=self.manager,
             container=self.action_container,
         )
         
         self.action_value = pygame_gui.elements.UILabel(
             relative_rect=pygame.Rect(margin, 0, -1, -1),
-            text='some action',
+            text='',
             manager=self.manager,
             container=self.action_container,
             anchors={
                 'left_target': self.action_label
             }
         )
-        
+
     def process_events(self, event: pygame.Event, env: Environment):
         if event.type == pygame_gui.UI_BUTTON_PRESSED:
-            match event.ui_element:
-                case self.sim_resume_btn:
-                    env.resume()
-                case self.sim_pause_btn:
-                    env.pause()
-                case self.sim_reset_btn:
-                    # TODO reset simulation
-                    pass
+            if event.ui_element == self.sim_resume_btn:
+                env.resume()
+            elif event.ui_element == self.sim_pause_btn:
+                env.pause()
+            elif event.ui_element == self.sim_reset_btn:
+                # TODO reset simulation
+                pass
+            elif event.ui_element == self.spawn_agent_btn:
+                self.change_brush(Agent, 'Agent')
+            elif event.ui_element in self.set_area_btns:
+               area = self.set_area_btns[event.ui_element]
+               self.change_brush(area, area.display_name)
+            elif event.ui_element in self.spawn_food_source_btns:
+                food_source = self.spawn_food_source_btns[event.ui_element]
+                self.change_brush(food_source, food_source.__name__)
+
+    def change_brush(self, new_brush, brush_name: str):
+        self.current_brush = new_brush
+        self.action_value.set_text(brush_name)
+
+        
